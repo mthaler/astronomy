c55b9cf2cd625e3e5e0b81d0f95e808f1104bcb5
Michael Thaler <michael.thaler@dlr.de>
2025-06-17 12:36:50 +0200

    Updated date.go and date_test.go.

--- date.go
+++ date.go
@@ -1,5 +1,7 @@
 package astronomy
 
+import "math"
+
 func Easter(y int) (int, int) {
 	a := y % 19
 	b := y / 100
@@ -17,4 +19,33 @@ func Easter(y int) (int, int) {
 	p := (h + l - 7*m + 114) % 31
 
 	return n, p + 1
+}
+
+func JulianDay(year, month int, day float64) float64 {
+	y := year
+	m := month
+	d := day
+	if m == 1 || m == 2 {
+		y = year - 1
+		m = month + 12
+	}
+	B := 0
+	if y >= 1582 {
+		if d >= 10 {
+			if d >= 15 {
+				A := int(y / 100)
+				B = 2 - A + int(A/4)
+			}
+		}
+	}
+	C := 0
+	if y < 0 {
+		C = int(math.Trunc((365.25 * float64(y)) - 0.75))
+	} else {
+		C = int(math.Trunc(365.25 * float64(y)))
+	}
+	D := int(30.6001 * float64(m+1))
+	JD := float64(B+C+D) + d + 1720994.5
+
+	return JD
 }
--- date_test.go
+++ date_test.go
@@ -11,3 +11,8 @@ func TestEaster(t *testing.T) {
 	assert.Equal(t, 4, month)
 	assert.Equal(t, 12, day)
 }
+
+func TestJulianDay(t *testing.T) {
+	jd := JulianDay(2009, 6, 19.75)
+	assert.Equal(t, 2455002.25, jd)
+}

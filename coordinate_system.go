7851fda12b8599119e199f06bf111ae666b2904d
Michael Thaler <michael.thaler@dlr.de>
2025-06-18 11:20:21 +0200

    Updated coordinate_system.go, coordinate_system_test.go and sun.go.

--- coordinate_system.go
+++ coordinate_system.go
@@ -1,9 +1,9 @@
 package astronomy
 
-func DecimalDegrees(d, m, s int) float64 {
+func DecimalDegrees(h, m, s int) float64 {
 	S := float64(s) / 60.0
 	M := (float64(m) + S) / 60.0
-	return float64(d) + M
+	return float64(h) + M
 }
 
 func DecimalDegreesToDegreeHourMinute(d float64) (int, int, int) {
@@ -11,6 +11,20 @@ func DecimalDegreesToDegreeHourMinute(d float64) (int, int, int) {
 	M := (d - float64(int(d))) * 60.0
 	S := (M - float64(int(M))) * 60.0
 	return D, int(M), int(S)
+}
+
+func DecimalDegreesToDeciamlHours(d float64) float64 {
+	return d / 15.0
+}
+
+func DecimalHoursToDecimalDegrees(d float64) float64 {
+	return d * 15
+}
+
+func RightAscension(h, m, s int) (int, int, int) {
+	D := DecimalDegreesToDeciamlHours(DecimalDegrees(h, m, s))
+	h, m, s = DecimalDegreesToDegreeHourMinute(D)
+	return h, m, s
 }
 
 func EquatorialToHorizontal(h, H, d float64) (int, int, int) {
--- coordinate_system_test.go
+++ coordinate_system_test.go
@@ -11,8 +11,15 @@ func TestDecinalDegreest(t *testing.T) {
 }
 
 func TestDecimalDegreesToDegreeHourMinute(t *testing.T) {
-	d, m, s := DecimalDegreesToDegreeHourMinute(182.52416666666667)
-	assert.Equal(t, 182, d)
+	h, m, s := DecimalDegreesToDegreeHourMinute(182.52416666666667)
+	assert.Equal(t, 182, h)
 	assert.Equal(t, 31, m)
 	assert.Equal(t, 27, s)
+}
+
+func TestRightAscension(t *testing.T) {
+	h, m, s := RightAscension(9, 36, 10)
+	assert.Equal(t, 144, h)
+	assert.Equal(t, 2, m)
+	assert.Equal(t, 33, s)
 }

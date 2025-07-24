a215d2491e49e61908cb303feff3247edaa6ea40
Michael Thaler <michael.thaler@dlr.de>
2025-07-24 13:33:57 +0200

    Updated celestial_object.go.

--- celestial_object.go
+++ celestial_object.go
@@ -1,5 +1,10 @@
 package astronomy
 
-func AngleBetween(alh, alm int, als float64, adh, adm int, ads float64, blh, blm int, bls float64, bdh, bdm int, bds float64) {
+import "fmt"
 
+func AngleBetween(alh, alm int, als float64, adh, adm int, ads float64, blh, blm int, bls float64, bdh, bdm int, bds float64) {
+	a1 := DecimalHour(alh, alm, als)
+	fmt.Println(a1)
+	d1 := DecimalHour(blh, blm, bls)
+	fmt.Println(d1)
 }

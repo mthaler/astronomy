f120b073f81c16e6b9577115b7bf123e9e6b61b2
Michael Thaler <michael.thaler@dlr.de>
2025-07-24 16:06:12 +0200

    Renamed celestial_object.go and celestial_object_test.go.

--- objects.go
+++ objects.go
@@ -2,6 +2,7 @@ package astronomy
 
 import (
 	"fmt"
+	"math"
 )
 
 func AngleBetween(alh, alm int, als float64, adh, adm int, ads float64, blh, blm int, bls float64, bdh, bdm int, bds float64) {
@@ -14,8 +15,11 @@ func AngleBetween(alh, alm int, als float64, adh, adm int, ads float64, blh, blm
 }
 
 func RisingSetting(ah, am int, as float64, dd, dm int, ds float64, y, m, d int) {
+	nu := 34.0 / 60.0
 	a := DecimalHour(ah, am, as)
 	fmt.Println(a)
 	de := DecimalDegrees(dd, dm, ds)
 	fmt.Println(de)
+	cosH := (math.Sin(nu*math.Pi/180.0 + math.Sin(a*math.Pi/180.0)*math.Sin(de*math.Pi/180.0))) / (math.Cos(a*math.Pi/180.0) * math.Cos(de*math.Pi/180.0))
+	fmt.Println(cosH)
 }

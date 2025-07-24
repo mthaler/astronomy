21109c0b75f4a2a0e3a9eaa473a8f7542f25f9ba
Michael Thaler <michael.thaler@dlr.de>
2025-07-24 11:11:13 +0200

    Updated coordinate_system.go again.

--- coordinate_system.go
+++ coordinate_system.go
@@ -116,5 +116,8 @@ func EquatorialToGalactic(ah, am int, as float64, dd, dm int, ds float64) (int, 
 }
 
 func GalacticToEquatorial(ld, lm int, ls float64, bd, bm int, bs float64) {
-
+	l := DecimalDegrees(ld, lm, ls)
+	b := DecimalDegrees(bd, bm, bs)
+	sind := math.Cos(b*math.Pi/180.0) / math.Cos(27.4*math.Pi/180.0) * (math.Sin((l-33.0)*math.Pi/180.0) + math.Sin(b*math.Pi/180.0)*math.Sin(27.4*math.Pi/180.0))
+	fmt.Println(sind)
 }

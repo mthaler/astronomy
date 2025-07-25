813b916dfd14e10995964338da60daddb2fc3f1b
Michael Thaler <michael.thaler@dlr.de>
2025-07-25 15:08:09 +0200

    Updated utils_test.go.

--- utils_test.go
+++ utils_test.go
@@ -1,7 +1,12 @@
 package astronomy
 
-import "testing"
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNormalizeTime(t *testing.T) {
-
+	assert.Equal(t, math.Pi, normalizeTime(180*math.Pi/180.0))
 }

f19c433ba6ef20e34a6fb65c29af821b4b65c962
Michael Thaler <michael.thaler@dlr.de>
2025-07-25 16:59:25 +0200

    Updated utils_test.go.

--- utils_test.go
+++ utils_test.go
@@ -11,3 +11,8 @@ func TestNormalizeTime(t *testing.T) {
 	assert.Equal(t, 12.0, normalizeTime(36))
 	assert.Equal(t, 12.0, normalizeTime(-12))
 }
+
+func TestIsInteger(t *testing.T) {
+	assert.Equal(t, false, isInteger(12.5))
+	assert.Equal(t, true, isInteger(12.999999999))
+}

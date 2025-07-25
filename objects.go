b695e8270bb25c11430a6f5bb3b5b0a3a23c2105
Michael Thaler <michael.thaler@dlr.de>
2025-07-25 09:34:05 +0200

    Updated objects.go.

--- objects.go
+++ objects.go
@@ -45,9 +45,12 @@ func Nutation(y, m int, d float64) {
 	T := (JD - 2415020.0) / 36525.0
 	A := 100.002136 * T
 	L := 279.6967 + 360.0*(A-float64(int(A))) - 360.0
-	fmt.Println(L)
 	B := 5.372617 * T
-	fmt.Println(B)
+	O := 259.1833 - 360.0*(B-float64(int(B))) + 360.0
+	DP := -17.2*math.Sin(O*math.Pi/180.0) - 1.3*math.Sin(2*L*math.Pi/180.0)
+	fmt.Println(DP)
+	De := 9.2*math.Cos(O*math.Pi/180.0) + 0.5*math.Cos(2*L*math.Pi/180.0)
+	fmt.Println(De)
 }
 
 func Aberration() {

146d0a8fd808ebe43ce5074bb2dd1aaadb104d30
Michael Thaler <michael.thaler@dlr.de>
2025-07-25 09:34:41 +0200

    Updated objects.go.

--- objects.go
+++ objects.go
@@ -40,7 +40,7 @@ func Precession(ah, am int, as float64, dd, dm int, ds float64) {
 	fmt.Println(s)
 }
 
-func Nutation(y, m int, d float64) {
+func Nutation(y, m int, d float64) (float64, float64) {
 	JD := JulianDay(y, m, d)
 	T := (JD - 2415020.0) / 36525.0
 	A := 100.002136 * T
@@ -48,9 +48,8 @@ func Nutation(y, m int, d float64) {
 	B := 5.372617 * T
 	O := 259.1833 - 360.0*(B-float64(int(B))) + 360.0
 	DP := -17.2*math.Sin(O*math.Pi/180.0) - 1.3*math.Sin(2*L*math.Pi/180.0)
-	fmt.Println(DP)
 	De := 9.2*math.Cos(O*math.Pi/180.0) + 0.5*math.Cos(2*L*math.Pi/180.0)
-	fmt.Println(De)
+	return DP, De
 }
 
 func Aberration() {

4368e9823a547b3bbe7ab5762a78fabdeb202995
Michael Thaler <michael.thaler@dlr.de>
2025-07-28 15:14:06 +0200

    Added angle_test.go.

--- coordinate_system.go
+++ coordinate_system.go
@@ -5,45 +5,6 @@ import (
 	"math"
 )
 
-func DecimalDegrees(h, m int, s float64) float64 {
-	S := s / 60.0
-	M := (float64(m) + float64(S)) / 60.0
-	if h > 0 {
-		return float64(h) + M
-	} else {
-		return float64(h) - M
-	}
-}
-
-func DecimalDegreesToDegreeHourMinute(d float64) (int, int, float64) {
-	D := int(d)
-	fmt.Println(D)
-	M := (d - math.Floor(d)) * 60.0
-	S := (M - math.Floor(M)) * 60.0
-	return D, int(M), S
-}
-
-func DecimalDegreesToDeciamlHours(d float64) float64 {
-	return d / 15.0
-}
-
-func DecimalHoursToDecimalDegrees(d float64) float64 {
-	return d * 15
-}
-
-func RightAscension(h, m int, s float64) (int, int, float64) {
-	D := DecimalHoursToDecimalDegrees(DecimalDegrees(h, m, s))
-	H, M, S := DecimalDegreesToDegreeHourMinute(D)
-	return H, M, S
-}
-
-func HourAngle(h, m int, s float64) (int, int, float64) {
-
-	D := DecimalDegreesToDeciamlHours(DecimalDegrees(h, m, s))
-	H, M, S := DecimalDegreesToDegreeHourMinute(D)
-	return H, M, S
-}
-
 func EquatorialToHorizontal(h, m, s, hh, mm, ss int, l float64) (int, int, float64, int, int, float64) {
 	H := DecimalHour(h, m, float64(s))
 	H = H * 15.0

08584e00444d6041b2e614acf8413e9584b2e190
Michael Thaler <michael.thaler@dlr.de>
2025-07-28 15:19:00 +0200

    Updated objects.go.

--- objects.go
+++ objects.go
@@ -8,11 +8,11 @@ import (
 func AngleBetween(alh, alm int, als float64, adh, adm int, ads float64, blh, blm int, bls float64, bdh, bdm int, bds float64) {
 	a1 := DecimalHour(alh, alm, als)
 	fmt.Println(a1)
-	d1 := DecimalHour(adh, adm, ads)
+	d1 := DecimalDegrees(adh, adm, ads)
 	fmt.Println(d1)
 	a2 := DecimalHour(blh, blm, bls)
 	fmt.Println(a2)
-	d2 := DecimalHour(bdh, bdm, bds)
+	d2 := DecimalDegrees(bdh, bdm, bds)
 	fmt.Println(d2)
 
 }

10bc94949e364d58f450fb0c7fe3b9a01dbea957
Michael Thaler <michael.thaler@dlr.de>
2025-07-29 11:27:03 +0200

    Updated kepler.go.

--- kepler.go
+++ kepler.go
@@ -1,8 +1,24 @@
 package astronomy
 
-/*
-The Kepler equation is E - e sin E = M
-*/
-func kepler() {
+import "math"
 
+/*
+ * To find a solution to Kepler’s equation E − e sin E = M for small values of e. All angles are
+ * expressed in radians.
+ */
+func kepler(E, e, M float64) float64 {
+	d := M - e*math.Sin(M)
+	ep := 0.000001
+	if math.Abs(d) <= ep {
+		return d
+	} else {
+		for {
+			DE := d / (1 - e*math.Cos(E))
+			E1 := E - DE
+			d := E1 - e*math.Sin(E1)
+			if math.Abs(d) <= ep {
+				return d
+			}
+		}
+	}
 }

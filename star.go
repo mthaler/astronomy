865eb2b32b7f493468f21b453bb196c21f305640
Michael Thaler <michael.thaler@dlr.de>
2025-07-29 10:31:00 +0200

    Updated star.go.

--- star.go
+++ star.go
@@ -1,11 +1,16 @@
 package astronomy
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 const StarEpoch = 1934.008
 
 func BinaryStarOrbit(y, m, d int, T float64) {
 	Y := float64(y) - StarEpoch
 	M := 360.0 * Y / T
+	M = normalizeAngle(M)
+	M = M * math.Pi / 180.0
 	fmt.Println(M)
 }

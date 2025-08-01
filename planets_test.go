4a6fb694cc59a59c0efd062fe14a5e60701ec726
Michael Thaler <michael.thaler@dlr.de>
2025-08-01 08:53:05 +0200

    Updated planets_test.go.

--- planets_test.go
+++ planets_test.go
@@ -3,5 +3,5 @@ package astronomy
 import "testing"
 
 func TestPlanetCoordinates(t *testing.T) {
-	PlanetCoordinates(2003, 11, 22.0, 11.8579, 337.917132, 14.6633)
+	PlanetCoordinates(2003, 11, 22.0, 11.8579, 337.917132, 14.6633, 0.048907)
 }

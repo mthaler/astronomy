6854e1fe6413bd8ff5110a50dcd3a36fcc22ac78
Michael Thaler <michael.thaler@dlr.de>
2025-08-01 09:09:46 +0200

    Updated planets.go and planets_test.go.

--- planets.go
+++ planets.go
@@ -5,14 +5,15 @@ import (
 	"math"
 )
 
-func PlanetCoordinates(y, m int, d, Tp, ep, o, e float64) {
+func PlanetCoordinates(y, m int, d, Tp, ep, o, e, a float64) {
 	D := JulianDay(y, m, d) - JulianDay(2010, 1, 0.0)
 	Np := 360.0 / 365.242191 * D / Tp
 	Np = normalizeAngle(Np)
 	Mp := Np + ep - o
 	nup := Mp + 360.0/math.Pi*e*math.Sin(Mp*math.Pi/180.0)
 	nup = normalizeAngle(nup)
-	fmt.Println(nup)
+	r := a * (1 - e*e) / (1 + e*math.Cos(nup*math.Pi/180.0))
+	fmt.Println(r)
 }
 
 func PlanetDistance() {
--- planets_test.go
+++ planets_test.go
@@ -3,5 +3,5 @@ package astronomy
 import "testing"
 
 func TestPlanetCoordinates(t *testing.T) {
-	PlanetCoordinates(2003, 11, 22.0, 11.8579, 337.917132, 14.6633, 0.048907)
+	PlanetCoordinates(2003, 11, 22.0, 11.8579, 337.917132, 14.6633, 0.048907, 5.20278)
 }

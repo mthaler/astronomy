08740f8bd09da9a14b15b4be0e2098471b813a18
Michael Thaler <michael.thaler@dlr.de>
2025-08-02 15:20:58 +0200

    Updated planets.go and planets_test.go.

--- planets.go
+++ planets.go
@@ -32,10 +32,16 @@ func PlanetCoordinates(y, m int, d, Tp, ep, o, e, a, i, O float64) {
 	yy := math.Sin((Lp-O)*math.Pi/180.0) * math.Cos(i*math.Pi/180.0)
 	x := math.Cos((Lp - O) * math.Pi / 180.0)
 	l := math.Atan(yy/x)*180.0/math.Pi + O
+	fmt.Println(l)
 	r2 := r * math.Cos(P*math.Pi/180.0)
 	la := math.Atan(R*math.Sin((l-Lp)*math.Pi/180.0)/(r2-R*math.Cos((l-Lp)*math.Pi/180.0)))*180/math.Pi + l
 	la = normalizeAngle(la)
 	fmt.Println(la)
+	fmt.Println(R * math.Sin((l-Lp)*math.Pi/180.0) / (r2 - R*math.Cos((l-Lp)*math.Pi/180.0)))
+}
+
+func PlanetPerturbation() {
+
 }
 
 func PlanetDistance() {

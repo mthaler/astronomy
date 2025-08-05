7172698eda54addb9b179be84516ed5e3aa43de0
Michael Thaler <michael.thaler@dlr.de>
2025-08-05 10:06:34 +0200

    Updated planets_test.go.

--- planets_test.go
+++ planets_test.go
@@ -10,7 +10,7 @@ func TestPlanetCoordinates(t *testing.T) {
 	ah, am, as, dd, dm, ds := PlanetCoordinates(2003, 11, 22.0, 11.8579, 337.917132, 14.6633, 0.048907, 5.20278, 1.3035, 100.595)
 	assert.Equal(t, 11, ah)
 	assert.Equal(t, 11, am)
-	assert.Equal(t, 14.0, as)
+	assert.Equal(t, 14.077566279029696, as)
 	assert.Equal(t, 6, dd)
 	assert.Equal(t, 21, dm)
 	assert.Equal(t, 25.0, ds)

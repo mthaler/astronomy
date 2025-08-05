060b3473ec5bb05fa8c3b13ac0e935d525a5ea5d
Michael Thaler <michael.thaler@dlr.de>
2025-08-05 10:02:38 +0200

    Updated planets_test.go.

--- planets_test.go
+++ planets_test.go
@@ -1,9 +1,19 @@
 package astronomy
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestPlanetCoordinates(t *testing.T) {
-	PlanetCoordinates(2003, 11, 22.0, 11.8579, 337.917132, 14.6633, 0.048907, 5.20278, 1.3035, 100.595)
+	ah, am, as, dd, dm, ds := PlanetCoordinates(2003, 11, 22.0, 11.8579, 337.917132, 14.6633, 0.048907, 5.20278, 1.3035, 100.595)
+	assert.Equal(t, 11, ah)
+	assert.Equal(t, 11, am)
+	assert.Equal(t, 14.0, as)
+	assert.Equal(t, 6, dd)
+	assert.Equal(t, 21, dm)
+	assert.Equal(t, 25.0, ds)
 }
 
 func TestPlanetPerturbation(t *testing.T) {

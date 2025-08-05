5451fc722adc65336bcf3f5f8e12bf09d69e7052
Michael Thaler <michael.thaler@dlr.de>
2025-08-05 12:36:49 +0200

    Updated earth.go.

--- earth.go
+++ earth.go
@@ -26,6 +26,7 @@ func Nutation(y, m int, d float64) (float64, float64) {
 	T := (JD - 2415020.0) / 36525.0
 	A := 100.002136 * T
 	L := 279.6967 + 360.0*(A-float64(int(A))) - 360.0
+	L = normalizeAngle(L)
 	B := 5.372617 * T
 	O := 259.1833 - 360.0*(B-float64(int(B))) + 360.0
 	DP := -17.2*math.Sin(O*math.Pi/180.0) - 1.3*math.Sin(2*L*math.Pi/180.0)
